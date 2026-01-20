d98fa688f94996d1b2a54e9c6bbaf0a7c75bf703
Esad <esat0738@gmail.com>
2026-01-21 01:14:45 +0300

    Otomatik guncelleme: Çar 21.01.2026  1:14:45,44

--- git.yap.bat
+++ git.yap.bat
@@ -1,5 +1,12 @@
 @echo off
+echo Islem baslatiliyor...
+:: 1. Once uzak sunucudaki degisiklikleri cek (Hata onleyici)
+git pull origin main
+:: 2. Tum yeni dosyalari ekle
 git add .
+:: 3. Otomatik mesajla kaydet
 git commit -m "Otomatik guncelleme: %date% %time%"
+:: 4. Kodlari gonder
 git push origin main
+echo Islem basariyla tamamlandi!
 pause
